f876e813fe4b67a693820660e2ca72ff6326ad44
yuriy.bykov <urlbin@yandex.ru>
2021-05-14 17:07:20 +0300

    поправили задачу: "Матрица". Откорректировали вывод, как в задании.

--- Матрица.py
+++ Матрица.py
@@ -1,15 +1,15 @@
-# Input lines:
-# 0 1 2 3
-# 4 5 6 7
-# 8 9 10 11
-# 12 13 14 15
+# Input lines with tabs:
+# 0    1    2    3
+# 4    5    6    7
+# 8    9    10    11
+# 12    13    14    15
 
 matrix = list()
 for i in range(1, 5):
     matrix.append(input())
 for j in matrix:
-    out_line = list()
-    split_line = j.split()
+    split_line = j.split(sep='\t')
     for k in split_line:
-        out_line.append(int(k) ** 2)
-    print(out_line)
+        out_number = int(k) ** 2
+        print(out_number, '\t', sep='', end='')
+    print('')
