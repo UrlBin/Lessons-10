206bf0cce69e522be86f5d522f7347556e8e2e9c
UrlBin <urlbin@yandex.ru>
2021-05-17 22:42:25 +0300

    Сделали задачу "Расшифровка строк в файле".

--- Бинарный файл строк/Расшифровка строк в файле.py
+++ Бинарный файл строк/Расшифровка строк в файле.py
@@ -7,12 +7,17 @@ sd
 ffc34
 '''
 
-file_name = 'binary_string.bin'
+file_name = input('Введите название файла. Если название не введено, то будет открыт ранее созданный файл.')
+if not file_name:
+    file_name = 'binary_string.bin'
 
 with open(file_name, mode='rb') as opened_file:
-    while True:
-        number_of_lines = opened_file.read(4)
-        if not number_of_lines:
-            break
-        else:
-            print("Декодированное число из файла --- ", int.from_bytes(line, byteorder='little'))
+    number_of_lines = int.from_bytes(opened_file.read(4), byteorder='little')
+    print(f'В прочитанном фале {number_of_lines} строк.')
+    for i in range(0, number_of_lines):
+        output_string = ''
+        length_of_next_line = int.from_bytes(opened_file.read(4), byteorder='little')
+        print(f'Длина строки № {i + 1} - {length_of_next_line} символов.')
+        for j in range(0, length_of_next_line):
+            output_string += chr(int.from_bytes(opened_file.read(2), byteorder='little'))
+        print(f"Значение строки № {i + 1} из файла -> ", output_string)
